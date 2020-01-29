5a2afacf872d5388386cf8faadcb4e9fc4a45fc3
Jess220494 <jess.charlton94@gmail.com>
2020-01-28 16:13:04 -0800

    backend task nested functions

---
+++ Backend/test.py
@@ -0,0 +1,30 @@
+# def ozzy():
+#     print("christmas")
+#     chester = "treats"
+#     test = 2
+#     test2 = ["sun", "moon", "star"]
+#     print(chester)
+#     print(test)
+#     print(test2)
+
+# ozzy()
+
+def function5(): 
+    def function4():
+        def function1():
+            one = "test"
+            print(one)
+        function1()
+
+        def function2():
+            test = 2
+            print(test)
+        function2()
+
+        def function3():
+            three = ["sun", "moon", "star"]
+            print(three)
+        function3()
+    function4()
+function5()
+

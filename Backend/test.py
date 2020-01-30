7287bf5091c0962c4f5a42b8188f548955ef47e2
Jess220494 <jess.charlton94@gmail.com>
2020-01-29 18:57:24 -0800

    simple solution

--- Backend/test.py
+++ Backend/test.py
@@ -9,22 +9,39 @@
 
 # ozzy()
 
-def function5(): 
-    def function4():
-        def function1():
-            one = "test"
-            print(one)
-        function1()
+# def function5(): 
+#     def function4():
+#         def function1():
+#             one = "test"
+#             print(one)
+#         function1()
 
-        def function2():
-            test = 2
-            print(test)
-        function2()
+#         def function2():
+#             test = 2
+#             print(test)
+#         function2()
 
-        def function3():
-            three = ["sun", "moon", "star"]
-            print(three)
-        function3()
+#         def function3():
+#             three = ["sun", "moon", "star"]
+#             print(three)
+#         function3()
+#     function4()
+# function5()
+
+def function1():
+    chester = "squaids"
+    return chester
+def function2():
+    rev = 2
+    return rev
+def function3():
+    day = ["sun", "moon", "star"]
+    return day
+def function4():
+    print(function1())
+    print(function2())
+    print(function3())
+def function5():
     function4()
 function5()
 

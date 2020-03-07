9084ab407b682c567673f3c5354c7271dcb206b3
Jess220494 <jess.charlton94@gmail.com>
2020-03-07 10:56:05 -0800

    login page functionality

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -24,22 +24,56 @@ import React, {Component} from 'react';
 import * as login_object from '../api/login';
 
 class Login extends Component {
-    async componentDidMount(){
-        console.log(await login_object.getProfile('user123', 'password123'))
+    state = {
+        username: '', 
+        password: '',
+        backend_output: ''
     }
+    async componentDidMount(){
+        console.log('whatever')
+    }
+    // handleOnInputUsername(event){
+
+    // }
+    // handleOnInputPassword(event){
+
+    // }
+    // handleOnClick(){
+
+    // }
+    handleOnInputUsername = (event) => {
+        this.setState({username: event})
+    }
+    
+    handleOnInputPassword = (event) => {
+        this.setState({password: event})
+    }
+
+    handleOnClick = async() => {
+        let output = await login_object.getProfile(this.state.username, this.state.password)
+        this.setState({backend_output: output})
+    }
+
+    printOutput = ()=> {
+        return this.state.backend_output
+    }
+
     render(){
         return(
             <div>
                 <IonItem>
                     <IonLabel position="floating">Username</IonLabel>
-                    <IonInput></IonInput>
+                    <IonInput onIonChange={(e)=>this.handleOnInputUsername(e.target.value)}></IonInput>
                 </IonItem>
                 <IonItem>
                     <IonLabel position="floating">Password</IonLabel>
-                    <IonInput></IonInput>
+                    <IonInput onIonChange={(e)=>this.handleOnInputPassword(e.target.value)}></IonInput>
                 </IonItem>
-                <IonButton>Login Now</IonButton>
-                
+                <IonButton onClick={()=>this.handleOnClick()}>Login Now</IonButton>
+                {console.log(this.state.username)}
+                {console.log(this.state.password)}
+                {console.log(this.state.backend_output)}  
+
             </div>
         )
     }
